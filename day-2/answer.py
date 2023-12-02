2d184b7618655f90c4e98c7257d1e91c8653dfc2
genghis <genghis.philip@gmail.com>
2023-12-01 23:21:58 -0600

    day2 ezpz

--- day-2/answer.py
+++ day-2/answer.py
@@ -1,11 +1,38 @@
 from aocd import get_data
-dataset = get_data(day=2, year=2023)
+dataset = get_data(day=2, year=2023).splitlines()
 
 def first():
-    pass
+    games = []
+    for i in dataset:
+        maxes = {"red": 0, "green": 0, "blue": 0}
+        num, turns = i.split(": ")
+        num = num.replace("Game ", "")
+        turnlist = turns.split("; ")
+        for j in turnlist:
+            draws = j.split(", ")
+            for k in draws:
+                number, color = k.split(" ")
+                if maxes[color] < int(number):
+                    maxes[color] = int(number)
+        if maxes["red"] <= 12 and maxes["green"] <= 13 and maxes["blue"] <= 14:
+            games.append(int(num))
+    print(sum(games))
 
 def second():
-    pass
+    games = []
+    for i in dataset:
+        maxes = {"red": 0, "green": 0, "blue": 0}
+        num, turns = i.split(": ")
+        num = num.replace("Game ", "")
+        turnlist = turns.split("; ")
+        for j in turnlist:
+            draws = j.split(", ")
+            for k in draws:
+                number, color = k.split(" ")
+                if maxes[color] < int(number):
+                    maxes[color] = int(number)
+        games.append(maxes["green"]*maxes["blue"]*maxes["red"])
+    print(sum(games))
 
 if __name__ == "__main__":
     first()
