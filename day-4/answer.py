7bdab781611c21c999d3fc852e841322a7c97bf4
genghis <genghis.philip@gmail.com>
2023-12-03 23:30:49 -0600

    30 min ain't so shabby

--- day-4/answer.py
+++ day-4/answer.py
@@ -1,11 +1,38 @@
 from aocd import get_data
-dataset = get_data(day=4, year=2023)
-
+dataset = get_data(day=4, year=2023).splitlines()
 def first():
-    pass
+    total = []
+    for card in dataset:
+        j = card.split(": ")
+        winners, mine = j[1].split(" | ")
+        winnerlist = [int(x) for x in winners.split(" ") if x]
+        minelist = [int(x) for x in mine.split(" ") if x]
+        results = [x for x in winnerlist if x in minelist]
+        if results:
+            counter = 1
+        else:
+            counter = 0
+        for i in range(len(results)-1):
+            counter *= 2
+        total.append(counter)
+    print(f"FIRST: {sum(total)}")
+
 
 def second():
-    pass
+    cardnumbers = {k: 1 for (k,v) in enumerate(dataset)}
+    for idx, card in enumerate(dataset):
+        j = card.split(": ")
+        winners, mine = j[1].split(" | ")
+        winnerlist = [int(x) for x in winners.split(" ") if x]
+        minelist = [int(x) for x in mine.split(" ") if x]
+        results = [x for x in winnerlist if x in minelist]
+        counter = 0
+        for i in range(len(results)):
+            counter += 1
+        for i in range(cardnumbers[idx]):
+            for j in range(1, counter+1):
+                cardnumbers[idx+j] += 1
+    print(f"SECOND: {sum(cardnumbers.values())}")
 
 if __name__ == "__main__":
     first()
