680cf35967999e18ecb607b4aac3f09c3baf8791
genghis <genghis.philip@gmail.com>
2023-12-03 23:57:53 -0600

    minorly better counter logic

--- day-4/answer.py
+++ day-4/answer.py
@@ -1,5 +1,6 @@
 from aocd import get_data
 dataset = get_data(day=4, year=2023).splitlines()
+
 def first():
     total = []
     for card in dataset:
@@ -17,7 +18,6 @@ def first():
         total.append(counter)
     print(f"FIRST: {sum(total)}")
 
-
 def second():
     cardnumbers = {k: 1 for (k,v) in enumerate(dataset)}
     for idx, card in enumerate(dataset):
@@ -26,12 +26,9 @@ def second():
         winnerlist = [int(x) for x in winners.split(" ") if x]
         minelist = [int(x) for x in mine.split(" ") if x]
         results = [x for x in winnerlist if x in minelist]
-        counter = 0
-        for i in range(len(results)):
-            counter += 1
-        for i in range(cardnumbers[idx]):
-            for j in range(1, counter+1):
-                cardnumbers[idx+j] += 1
+        counter = len(results)
+        for j in range(1, counter+1):
+            cardnumbers[idx+j] += (1*cardnumbers[idx])
     print(f"SECOND: {sum(cardnumbers.values())}")
 
 if __name__ == "__main__":
