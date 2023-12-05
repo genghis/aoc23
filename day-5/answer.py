4ed536e42970cd761dc1e9c15c61f0c80451fa10
genghis <genghis.philip@gmail.com>
2023-12-05 12:02:08 -0600

    initial push up with part 1

--- day-5/answer.py
+++ day-5/answer.py
@@ -1,8 +1,136 @@
 from aocd import get_data
-dataset = get_data(day=5, year=2023)
+from pprint import pprint
+
+dataset = get_data(day=5, year=2023).split('\n\n')
+# dataset = '''seeds: 79 14 55 13
+
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
+
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
+
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
+
+# water-to-light map:
+# 88 18 7
+# 18 25 70
+
+# light-to-temperature map:
+# 45 77 23
+# 81 45 19
+# 68 64 13
+
+# temperature-to-humidity map:
+# 0 69 1
+# 1 0 69
+
+# humidity-to-location map:
+# 60 56 37
+# 56 93 4'''.split('\n\n')
+
+
+datakeys = [y for y in [x.split(':')[0] for x in dataset]]
+datavalues = [y.strip().split('\n') for y in [x.split(':')[1] for x in dataset]]
+data = {k:v for (k,v) in zip(datakeys,datavalues)}
+
+mapping = {
+    'seed': {},
+    'soil': {},
+    'fertilizer': {},
+    'water': {},
+    'light': {},
+    'temperature': {},
+    'humidity': {},
+    'location': {}}
+
+def set_destinations(destination):
+    pass
+
+def set_source(source):
+    pass
+
+def break_maps(mapname):
+    usable = mapname.split(' ')[0]
+    destination = usable.split('-to-')[0]
+    source = usable.split('-to-')[1]
+    return (destination,source)
+    
+
+def set_mapping():
+    for mapname in data.keys():
+        if mapname != "seeds":
+            print(mapname)
+            tup = break_maps(mapname)
+            destination = tup[0]
+            source = tup[1]
+            for datum in data[mapname]:
+                # pprint(mapname)
+                # pprint(datum)
+                item = datum.split(' ')
+                deststart = int(item[0])
+                sourcestart = int(item[1])
+                rng = int(item[2])
+                dlist = [x for x in range(deststart, deststart+rng)]
+                slist = [x for x in range(sourcestart, sourcestart+rng)]
+                final = {k:v for (k,v) in zip(slist,dlist)}
+                mapping[destination].update(final)
+        # else:
+        #     for i in data['seeds'][0].split(' '):
+        #         mapping['seed'][int(i)] = 0
+        
 
 def first():
-    pass
+    set_mapping()
+    # pprint(mapping)
+    locations = []
+    for seed in data['seeds'][0].split(' '):
+        # print(f"Seed is {seed}")
+        if int(seed) in mapping['seed'].keys():
+            soil = mapping['seed'][int(seed)]
+        else:
+            soil = int(seed)
+        # print(f"Soil is {soil}")
+        if soil in mapping['soil'].keys():
+            fertilizer = mapping['soil'][soil]
+        else:
+            fertilizer = soil
+        # print(f"Fertilizer is {fertilizer}")
+        if fertilizer in mapping['fertilizer'].keys():
+            water = mapping['fertilizer'][fertilizer]
+        else:
+            water = fertilizer
+        # print(f"Water is {water}")
+        if water in mapping['water'].keys():
+            light = mapping['water'][water]
+        else:
+            light = water
+        # print(f"Light is {light}")
+        if light in mapping['light'].keys():
+            temperature = mapping['light'][light]
+        else:
+            temperature = light
+        # print(f"Temperature is {temperature}")
+        if temperature in mapping['temperature'].keys():
+            humidity = mapping['temperature'][temperature]
+        else:
+            humidity = temperature
+        # print(f"Humidity is {humidity}")
+        if humidity in mapping['humidity'].keys():
+            location = mapping['humidity'][humidity]
+        else:
+            location = humidity
+        # print(f"Location is {location}")
+        locations.append(location)
+    # print(locations)
+    print(min(locations))
 
 def second():
     pass
---
+++ day-5/scratch.py
@@ -0,0 +1,118 @@
+from aocd import get_data
+dataset = get_data(day=5, year=2023).split('\n\n')
+
+# dataset = '''seeds: 79 14 55 13
+
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
+
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
+
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
+
+# water-to-light map:
+# 88 18 7
+# 18 25 70
+
+# light-to-temperature map:
+# 45 77 23
+# 81 45 19
+# 68 64 13
+
+# temperature-to-humidity map:
+# 0 69 1
+# 1 0 69
+
+# humidity-to-location map:
+# 60 56 37
+# 56 93 4'''.split('\n\n')
+
+mapping = {}
+
+for i in dataset:
+    chunks = i.split(':')
+    if chunks[0] != "seeds":
+        name = chunks[0].split('-to-')[1].replace(" map", "")
+    else:
+        name = "seeds"
+    if len('chunks1') > 1:
+        entries = chunks[1].strip().split('\n')
+    else:
+        entries = chunks[1].strip()
+    mapping[name] = entries
+    
+def check(val,key):
+    for i in mapping[key]:
+        nums = i.split(' ')
+        prevstart = int(nums[0])
+        nextstart = int(nums[1])
+        rngmax = int(nums[2])
+        # print(f"Val = {val}\nRange = {prevstart}-")
+        if val in range(nextstart,nextstart+rngmax):
+            # print(f"{prevstart} through {prevstart+rngmax}")
+            offset = nextstart-prevstart
+            # print(f"Offset is {offset}")
+            return(val-offset)
+    return val 
+
+def process_backwards(val, key):
+    for i in mapping 
+    humidity = check(temperature, "humidity")
+    temperature = check(light, "temperature")
+    light = check(water, "light")
+    water = check(fertilizer, "water")
+    fertilizer = check(soil, "fertilizer")
+    soil = check(int(seed), "soil")
+    
+def process(seed):
+    print(f"Seed = {seed}")
+    soil = check(int(seed), "soil")
+    print(f"Soil = {soil}")
+    fertilizer = check(soil, "fertilizer")
+    print(f"Fertilizer = {fertilizer}")
+    water = check(fertilizer, "water")
+    print(f"Water = {water}")
+    light = check(water, "light")
+    print(f"Light = {light}")
+    temperature = check(light, "temperature")
+    print(f"Temperature = {temperature}")
+    humidity = check(temperature, "humidity")
+    print(f"Humidity = {humidity}")
+    location = check(humidity, "location")
+    print(f"Location = {location}")
+    return(location)
+    
+def first():
+    locations = []
+    seedlist = mapping['seeds'][0].split(' ')
+    for seed in seedlist:
+        locations.append(process(int(seed)))
+    print(min(locations))
+        
+
+def second():
+    locations = []
+    array = mapping['seeds'][0].split(' ')
+    counter = 0
+    seedlist = []
+    while counter < len(array):
+        base = int(array[counter])
+        rngmax = int(array[counter+1])
+        for i in range(base, base+rngmax):
+            seedlist.append(i)
+        counter += 2
+    for seed in seedlist:
+        locations.append(process(int(seed)))
+    print(min(locations))
+
+if __name__ == "__main__":
+    first()
+    second()
