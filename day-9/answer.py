84de624cd50c00a63d164272afd47408fabfccb6
genghis <genghis.philip@gmail.com>
2023-12-09 11:45:01 -0600

    ridiculous

--- day-9/answer.py
+++ day-9/answer.py
@@ -1,11 +1,25 @@
-from aocd import get_data
-dataset = get_data(day=9, year=2023)
+dataset = [[int(y) for y in x.split(' ')] for x in open('input.txt').read().splitlines()]
+
+def sequencer(line, direction):
+    if direction == 'back':
+        line.reverse()
+    counter = 0
+    while line:
+        counter += line[-1]
+        line = [b - a for a, b in zip(line[:-1], line[1:])]
+    return counter
 
 def first():
-    pass
+    results = []
+    for i in dataset:
+        results.append(sequencer(i, 'forward'))
+    print(f"PART ONE: {sum(results)}")
 
 def second():
-    pass
+    results = []
+    for i in dataset:
+        results.append(sequencer(i, 'back'))
+    print(f"PART TWO: {sum(results)}")
 
 if __name__ == "__main__":
     first()
